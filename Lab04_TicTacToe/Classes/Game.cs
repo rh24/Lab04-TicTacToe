cadc319c98602f0e43ca32215a7aec492ee2ee88
rebeccahong <rebeccayhong@gmail.com>
2018-10-12 09:32:20 -0700

    Add start game in game class

--- Lab04_TicTacToe/Classes/Game.cs
+++ Lab04_TicTacToe/Classes/Game.cs
@@ -4,36 +4,39 @@ using System.Text;
 
 namespace Lab04_TicTacToe.Classes
 {
-	class Game
-	{
-		public Player PlayerOne { get; set; }
-		public Player PlayerTwo { get; set; }
-		public Player Winner { get; set; }
-		public Board Board { get; set; }
+    class Game
+    {
+        public Player PlayerOne { get; set; }
+        public Player PlayerTwo { get; set; }
+        public Player Winner { get; set; }
+        public Board Board { get; set; }
 
 
-		/// <summary>
-		/// Require 2 players and a board to start a game. 
-		/// </summary>
-		/// <param name="p1">Player 1</param>
-		/// <param name="p2">Player 2</param>
-		public Game(Player p1, Player p2)
-		{
-			PlayerOne = p1;
-			PlayerTwo = p2;
-			Board = new Board();
-		}
+        /// <summary>
+        /// Require 2 players and a board to start a game. 
+        /// </summary>
+        /// <param name="p1">Player 1</param>
+        /// <param name="p2">Player 2</param>
+        public Game(Player p1, Player p2)
+        {
+            PlayerOne = p1;
+            PlayerTwo = p2;
+            if (p1 != null && p2 != null)
+            {
+                Board = new Board();
+            }
+        }
 
-		/// <summary>
-		/// Activate the Play of the game
-		/// </summary>
-		/// <returns>Winner</returns>
-		public Player Play()
-		{
+        /// <summary>
+        /// Activate the Play of the game
+        /// </summary>
+        /// <returns>Winner, which is a Player object.</returns>
+        public Player Play()
+        {
 
-			//TODO: Complete this method and utilize the rest of the class structure to play the game.
+            //TODO: Complete this method and utilize the rest of the class structure to play the game.
 
-			/*
+            /*
 			 While there isn't a winner determined or too many turns have been taken,
 			 allow each player to see the board and take a turn.
 			 A turn consists of picking a position on the board, and then putting their appropriate marker
@@ -41,76 +44,78 @@ namespace Lab04_TicTacToe.Classes
 			 board so the next player can accurately choose. 
 			 Once a winner is determined, display the board and return a winner 
 			 */
-		}
+
+            if (!CheckForWinner)
+        }
 
 
-		/// <summary>
-		/// Check if winner exists
-		/// </summary>
-		/// <param name="board">current state of the board</param>
-		/// <returns>if winner exists</returns>
-		public bool CheckForWinner(Board board)
-		{
-			int[][] winners = new int[][]
-			{
-				new[] {1,2,3},
-				new[] {4,5,6},
-				new[] {7,8,9},
+        /// <summary>
+        /// Check if winner exists
+        /// </summary>
+        /// <param name="board">current state of the board</param>
+        /// <returns>if winner exists</returns>
+        public bool CheckForWinner(Board board)
+        {
+            int[][] winners = new int[][]
+            {
+                new[] {1,2,3},
+                new[] {4,5,6},
+                new[] {7,8,9},
 
-				new[] {1,4,7},
-				new[] {2,5,8},
-				new[] {3,6,9},
+                new[] {1,4,7},
+                new[] {2,5,8},
+                new[] {3,6,9},
 
-				new[] {1,5,9},
-				new[] {3,5,7}
-			};
+                new[] {1,5,9},
+                new[] {3,5,7}
+            };
 
-			// Given all the winning conditions, Determine the winning logic. 
-			for (int i = 0; i < winners.Length; i++)
-			{
-				Position p1 = Player.PositionForNumber(winners[i][0]);
-				Position p2 = Player.PositionForNumber(winners[i][1]);
-				Position p3 = Player.PositionForNumber(winners[i][2]);
+            // Given all the winning conditions, Determine the winning logic. 
+            for (int i = 0; i < winners.Length; i++)
+            {
+                Position p1 = Player.PositionForNumber(winners[i][0]);
+                Position p2 = Player.PositionForNumber(winners[i][1]);
+                Position p3 = Player.PositionForNumber(winners[i][2]);
 
-				string a = Board.GameBoard[p1.Row, p1.Column];
-				string b = Board.GameBoard[p2.Row, p2.Column];
-				string c = Board.GameBoard[p3.Row, p3.Column];
+                string a = Board.GameBoard[p1.Row, p1.Column];
+                string b = Board.GameBoard[p2.Row, p2.Column];
+                string c = Board.GameBoard[p3.Row, p3.Column];
 
-				// TODO:  Determine a winner has been reached. 
-				// return true if a winner has been reached. 
-			
-			}
+                // TODO:  Determine a winner has been reached. 
+                // return true if a winner has been reached. 
 
-			return false;
-		}
+            }
 
-
-		/// <summary>
-		/// Determine next player
-		/// </summary>
-		/// <returns>next player</returns>
-		public Player NextPlayer()
-		{
-			return (PlayerOne.IsTurn) ? PlayerOne : PlayerTwo;
-		}
-
-		/// <summary>
-		/// End one players turn and activate the other
-		/// </summary>
-		public void SwitchPlayer()
-		{
-			if (PlayerOne.IsTurn)
-			{
-				PlayerOne.IsTurn = false;
-				PlayerTwo.IsTurn = true;
-			}
-			else
-			{
-				PlayerOne.IsTurn = true;
-				PlayerTwo.IsTurn = false;
-			}
-		}
+            return false;
+        }
 
 
-	}
+        /// <summary>
+        /// Determine next player
+        /// </summary>
+        /// <returns>next player</returns>
+        public Player NextPlayer()
+        {
+            return (PlayerOne.IsTurn) ? PlayerOne : PlayerTwo;
+        }
+
+        /// <summary>
+        /// End one players turn and activate the other
+        /// </summary>
+        public void SwitchPlayer()
+        {
+            if (PlayerOne.IsTurn)
+            {
+                PlayerOne.IsTurn = false;
+                PlayerTwo.IsTurn = true;
+            }
+            else
+            {
+                PlayerOne.IsTurn = true;
+                PlayerTwo.IsTurn = false;
+            }
+        }
+
+
+    }
 }
