294ec460a619177862bf723a0ace73b2c704d29f
rebeccahong <rebeccayhong@gmail.com>
2018-10-12 13:12:28 -0700

    Add first passing test

--- Lab04-Tests/UnitTest1.cs
+++ Lab04-Tests/UnitTest1.cs
@@ -1,14 +1,38 @@
 using System;
 using Xunit;
+using Lab04_TicTacToe.Classes;
 
 namespace Lab04_Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        /*
+            Given a game board, Test for winners (at least 3)
+            Test that there is a switch in players between turns
+            Confirm that the position the player inputs correlates to the correct index of the array
+            One other “unique” test of your own
+         */
 
+        Board board = new Board();
+        //board.GameBoard = new string[,] { { "X", "X", "X" }, { "4", "5", "6" }, { "7", "8", "9" } };
+        public static string[,] gameboard = new string[,]
+        {
+            {"1", "2", "3"},
+            {"4", "5", "6"},
+            {"7", "8", "9"},
+        };
+
+        [Fact]
+        public void DisplayBoard()
+        {
+            Assert.Equal(new string[,]
+            {
+                {"1", "2", "3"},
+                {"4", "5", "6"},
+                {"7", "8", "9"},
+            }, board.GameBoard);
         }
+
+
     }
 }
