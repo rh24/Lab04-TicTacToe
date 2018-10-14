4278e73d12cae358b6e4e3c3311c92facbe1e302
rebeccahong <rebeccayhong@gmail.com>
2018-10-13 17:39:56 -0700

    Add switched players passing test

--- Lab04-Tests/UnitTest1.cs
+++ Lab04-Tests/UnitTest1.cs
@@ -13,11 +13,17 @@ namespace Lab04_Tests
             One other “unique” test of your own
          */
 
+        /// <summary>
+        /// Instantiate new objects of each class in order to have test values to pass into methods
+        /// </summary>
         public Board board = new Board();
         public static Player p1 = new Player { Name = "Test", Marker = "X", IsTurn = true };
         public static Player p2 = new Player { Name = "Player", Marker = "O", IsTurn = false };
         public Game game = new Game(p1, p2);
 
+        /// <summary>
+        /// Check that the board is being created with proper values
+        /// </summary>
         [Fact]
         public void DisplayBoard()
         {
@@ -29,6 +35,10 @@ namespace Lab04_Tests
             }, board.GameBoard);
         }
 
+        /// <summary>
+        /// Test when no one wins
+        /// </summary>
+        /// <param name="expected"></param>
         [Theory]
         [InlineData(false)]
         public void CheckWinner1(bool expected)
@@ -38,6 +48,10 @@ namespace Lab04_Tests
             Assert.Equal(expected, game.CheckForWinner(board));
         }
 
+        /// <summary>
+        /// Test where X is the winner
+        /// </summary>
+        /// <param name="expected"></param>
         [Theory]
         [InlineData(true)]
         public void CheckWinner2(bool expected)
@@ -48,6 +62,10 @@ namespace Lab04_Tests
             Assert.Equal(expected, game.CheckForWinner(board));
         }
 
+        /// <summary>
+        /// Test when O is the winner
+        /// </summary>
+        /// <param name="expected"></param>
         [Theory]
         [InlineData(true)]
         public void CheckWinner3(bool expected)
@@ -56,6 +74,22 @@ namespace Lab04_Tests
             board.GameBoard = new string[,] { { "1", "X", "O" }, { "4", "X", "O" }, { "7", "8", "O" } };
 
             Assert.Equal(expected, game.CheckForWinner(board));
+        }
+
+        // Test that there is a switch in players between turns
+        /// <summary>
+        /// p1 is the starting player because its IsTurn property = true.
+        /// For this test to pass, p2.IsTurn should be true and p1.IsTurn should be false.
+        /// </summary>
+        /// <param name="expected"></param>
+        [Theory]
+        [InlineData(true)]
+        public void SwitchPlayers(bool expected)
+        {
+            game.SwitchPlayer();
+            bool switchedPlayers = p1.IsTurn == false && p2.IsTurn == true;
+
+            Assert.Equal(expected, switchedPlayers);
         }
     }
 }
